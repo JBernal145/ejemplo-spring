e4c3e204df0465426c76beb40c95a1d7e29704d3
Javier Bernal <javier.bernal@servexternos.gruposantander.com>
2021-11-15 15:44:40 +0100

    First commit

---
+++ src/test/java/com/santander/formacion/fullstack/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santander.formacion.fullstack.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
